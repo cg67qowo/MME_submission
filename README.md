784d13f73011d31bbf5b755c2e281ed0f53ef707
cg67qowo <cg67qowo@studserv.uni-leipzig.de>
2025-12-22 15:53:19 +0100

    added code

--- README.md
+++ README.md
@@ -1,3 +1,21 @@
 # MME_submission
 
 This repository contains the data and the code for a paper submitted to the MME workshop, taking place at EACL 2026.
+
+The repository is structured as follows:
+
+- data: this folder contains the data used for this paper. It is divided into:
+    - cleaned_FrenchPolArg: This is the post-processed version of the debates scraped for FrenchPolArg.
+    - data_for_training: This repository contains the data used for the various experiments. All the data is in CONLL format, with token-level annotations. The data comes from EN-ElecDeb60to20, FR-ElecDeb60to20, and FrenchPolArg (specifically its manually annotated sample).
+    - FrenchPolArg_speeches, containing the further material collected for FrenchPolArg. As mentioned in the paper, this material was not yet used for our research, because we focused on conversational data to make the results comparable to ElecDeb60to20, which is composed by debates.
+    - translated_elecdeb: This repository contains the translation of ElecDeb60to20 into French and its backtranslation into English, before completion of the projection pipeline. The annotation projection does not have a dedicated folder, because it corresponds to the training data and can be seen in the data_for_training folder.
+
+- component_detection: the code used to test the mentioned models on the component extraction task.
+
+- translation_and_projection: the code used to translated ElecDeb60to20 and project the annotations.
+
+- preprocessing: code for the various preprocessing steps. Not all the steps are reported here, as the paper does not focus on dataset building.
+
+As explained in the paper, this work partly reuses already existent works. In particular, the folder translation_and_projection comes from [https://github.com/ragerri/abstrct-projections/tree/main/cross-lingual-argument-mining](this repository by Agerri), which refers to the paper [Cross-Lingual Argument Mining in the Medical Domain (Yeginbergen and Agerri, 2024)](https://arxiv.org/abs/2301.10527), and component_detection comes from [this repository by Goffredo](https://github.com/pierpaologoffredo/PoliticalDebates_AM/tree/main). Component_detection was modified to include different architectures and setups tested in this paper, but keeps Goffredo's structure and implementation. **translation_and_projection was taken as is, and the only differences are related to adaptation to my data.** For these reasons, I reported the original licenses in the folders.
+
+This code is being submitted here for transparency, and before an eventual publication of the code I will make sure that the authors are properly cited in each file. To preserve anonimity, I did not upload the bash files I used to run the code on the cluster.
---
+++ translation_and_projection/translate_fr.py
@@ -0,0 +1,106 @@
+import argparse
+from tqdm import tqdm
+from easynmt import EasyNMT, models
+import deepl
+import fasttext
+
+
+def to_sentence(input_file, output_file):
+  '''
+  Collect sequences in .tsv file into lines and save it into .txt file
+  '''
+  sentence = ''
+  file = open(input_file, 'r')
+  write_file = open(output_file, 'w')
+  for line in file:
+      if line != '\n':
+        token = line.split(' ')[0]
+        sentence += token + ' '
+      else:
+        write_file.write(sentence + '\n')
+        sentence = ''
+  return write_file
+
+
+def translate_opus(source_file, output_file, target_language):
+
+  #  model_name='opus-mt'
+  #  model = EasyNMT(model_name)
+    model = EasyNMT(translator=models.AutoModel('Helsinki-NLP/opus-mt-en-fr'))
+    num_sentences = len(open(source_file).readlines())
+    translate_file=open(source_file, 'r')
+    print(translate_file)
+    write_translation = open(output_file, 'w')
+    
+    for _, text in zip(tqdm(range(num_sentences)) ,translate_file):
+        try:
+          write_translation.write(model.translate(text, target_lang=target_language))
+        except Exception as e:
+          print(f"Error on line {_}: {text}")
+          print(e)
+    
+    return
+
+def translate_deepl(source_file, output_file, target_language,auth_key):
+
+    translator = deepl.Translator(auth_key)
+    num_sentences = len(open(source_file, 'r').readlines())
+    file = open(source_file, 'r')
+    wr = open(output_file, 'w')
+
+    for _, line in zip(tqdm(range(num_sentences)),file):
+        translation = translator.translate_text(line,target_lang=target_language)
+        wr.write(translation.text)
+    
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        "--data_path",
+        type=str,
+        required=True,
+        help="Path to the .txt file to tranlsate",
+    )
+    parser.add_argument(
+        "--target_language",
+        type=str,
+        required=True,
+        help="The target language to translate the data",
+    )
+    parser.add_argument(
+        "--save_translation_file",
+        type=str,
+        required=True,
+        help="The file to write the transaltions",
+    )
+    parser.add_argument(
+      '--model',
+      type=str,
+      required=False,
+      help='Translation model: opus-mt or deepl'
+    )
+    parser.add_argument(
+      '--deepl_key',
+      type=str,
+      required=False,
+      default='',
+      help='Auth key for DeepL API'
+    )
+
+
+    args = parser.parse_args()
+
+    if args.model == 'opus-mt':
+      translate_opus(
+        source_file=args.data_path,
+        output_file=args.save_translation_file,
+        target_language=args.target_language
+      )
+    elif args.model == 'deepl':
+      translate_deepl(
+        source_file=args.data_path,
+        output_file=args.save_translation_file,
+        target_language=args.target_language,
+        auth_key=args.deepl_key
+      )
